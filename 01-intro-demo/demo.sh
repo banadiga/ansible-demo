40d16bede4fe25bfcb457458c8ddcd9056293b6b
Ihor Banadiga <banadiga@gmail.com>
2018-10-03 18:08:16 +0300

    intro demo added

---
+++ 01-intro-demo/demo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# step 1 install ansible
+sudo apt-get -y install ansible
+
+# step 2 check version
+ansible --version
+
+# step 3 install latest version
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get -y install ansible
+
+# step 4 check version
+ansible --version
+
+# step 5 ping localhost
+ansible localhost -m ping
+
+# step 6 check available commands
+#ansible \tab
