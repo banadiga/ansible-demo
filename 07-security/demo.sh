18f4eaf6c39ab87d4890c37e2b0eed8b73bf2316
Ihor Banadiga <banadiga@gmail.com>
2018-10-03 18:12:03 +0300

    security added

---
+++ 07-security/demo.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# step 1 run playbook
+ansible-playbook playbook.yml
+
+# step 2 encrypt vars
+ansible-vault encrypt vars.yaml
+
+# step 3 encrypt vars
+ansible-vault edit vars.yaml
+
+# step 4 run playbook
+ansible-playbook playbook.yaml --ask-vault-pass
+
+# step 5 encrypt vars
+ansible-vault encrypt_string 'sicret data from variable' -name SICRET1
+
+# step 6 replace SICRET1 and run playbook
+ansible-playbook playbook.yaml --ask-vault-pass
+
+# step 7 add no_log:true to task and run playbook
+ansible-playbook playbook.yaml --ask-vault-pass
+
+# step 8 encrypt vars
+ansible-vault decrypt vars.yaml
