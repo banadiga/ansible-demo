939786db7ea499d11123927af6a9462a059c4d25
Ihor Banadiga <banadiga@gmail.com>
2018-10-28 23:30:04 +0200

    fix roles demo

--- 05-roles/demo.sh
+++ 05-roles/demo.sh
@@ -11,6 +11,14 @@ tree
 # step 3 run use role
 ansible-playbook playbook.yml
 
-# step 4 cleanup
-rm -rf roles/show-message
+# step 4 add 3rd party role
+ansible-galaxy search apache --author=kbrebanov 
 
+# step 5 add 3rd party role
+ansible-galaxy install kbrebanov.java -c
+
+# step 6 run use role
+ansible-playbook -i inventory.ini playbook.yml
+
+# step 7 cleanup
+rm -rf roles/show-message
